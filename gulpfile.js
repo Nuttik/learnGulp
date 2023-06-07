b3c03651b57bfc08d309900326dba18611e9f3d9
Natalia Repkina <pn.88@mail.ru>
2023-06-07 12:09:21 +0300

    feat: add scss handler

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,23 @@ const { watch, series, dest, src, parallel} = require('gulp');
 // Подключаем compress-images для работы с изображениями
 const imagecomp = require('compress-images');
 
+// Определяем переменную "preprocessor"
+let preprocessor = 'sass'; 
+
+// Подключаем gulp-concat
+const concat = require('gulp-concat');
+
+// Подключаем модули gulp-sass и gulp-less
+const sass = require('gulp-sass')(require('sass'));
+const less = require('gulp-less');
+
+// Подключаем Autoprefixer
+const autoprefixer = require('gulp-autoprefixer');
+
+// Подключаем модуль gulp-clean-css
+const cleancss = require('gulp-clean-css');
+
+// Подключаем модуль gulp-clean
 var clean = require('gulp-clean');
 
 function html() {
@@ -18,6 +35,17 @@ function css() {
     .pipe(dest('build/assets/styles'))
     .pipe(browserSync.stream())
 }
+
+function styles() {
+	return src('src/assets/styles/' + preprocessor + '/main.' + preprocessor + '') // Выбираем источник: "app/sass/main.sass" или "app/less/main.less"
+	.pipe(eval(preprocessor)()) // Преобразуем значение переменной "preprocessor" в функцию
+	.pipe(concat('app.min.css')) // Конкатенируем в файл app.min.js
+	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true })) // Создадим префиксы с помощью Autoprefixer
+	.pipe(cleancss( { level: { 1: { specialComments: 0 } }/* , format: 'beautify' */ } )) // Минифицируем стили
+	.pipe(dest('build/assets/styles/')) // Выгрузим результат в папку "app/css/"
+	.pipe(browserSync.stream()) // Сделаем инъекцию в браузер
+}
+
 function fonts() {
     return src('src/assets/fonts/**/*')
     .pipe(dest('build/assets/fonts/'))
@@ -54,6 +82,7 @@ function startWatch() {
     watch('src/assets/styles/**/*.css', css)
     watch('src/assets/images/**/*', images)
     watch('src/assets/fonts/**/*', fonts)
+	watch('src/assets/styles/**/' + preprocessor + '/**/*', styles);
 }
 
 function clear(){
@@ -61,10 +90,10 @@ function clear(){
     .pipe(clean());
 }
 
-exports.dev = parallel(browsersync, startWatch, html, css, images, fonts)
-exports.build = series(clear, parallel(html, css, images, fonts))
+exports.dev = parallel(browsersync, startWatch, html, css, images, fonts, styles)
+exports.build = series(clear, parallel(html, css, images, fonts, styles))
 
 
-exports.default = parallel(browsersync, startWatch, html, css, images, fonts)
+exports.default = parallel(browsersync, startWatch, html, css, images, fonts, styles)
 
   
