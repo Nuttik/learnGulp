c19a6e8da5eea93e904a47785059f8570373df04
Natalia Repkina <41197830+Nuttik@users.noreply.github.com>
2023-06-09 19:52:37 +0300

    feat: add scss handler (#2)
    * feat: add scss handler
    
    * fix(gulp): change config
    
    ---------
    
    Co-authored-by: Aleksey Drozdov <leshadrozdov@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,90 @@
-const browserSync = require('browser-sync');
+const browserSync = require('browser-sync')
 
-const { watch, series, dest, src, parallel} = require('gulp');
+const { watch, series, dest, src, parallel } = require('gulp')
 
 // Подключаем compress-images для работы с изображениями
-const imagecomp = require('compress-images');
+const imagecomp = require('compress-images')
 
-var clean = require('gulp-clean');
+// Подключаем модули gulp-sass и gulp-less
+const sass = require('gulp-sass')(require('sass'))
 
-function html() {
-    return src('src/index.html')
+// Подключаем Autoprefixer
+const autoprefixer = require('gulp-autoprefixer')
+
+// Подключаем модуль gulp-clean-css
+const cleancss = require('gulp-clean-css')
+
+// Подключаем модуль gulp-clean
+var clean = require('gulp-clean')
+
+function html()
+{
+  return src('src/index.html')
     .pipe(dest('build'))
     .pipe(browserSync.stream())
 }
 
-function css() {
-    return src('src/assets/styles/index.css')
-    .pipe(dest('build/assets/styles'))
-    .pipe(browserSync.stream())
+function css()
+{
+  return src('src/assets/styles/**/*.scss') // берём все файлы со стилями
+		.pipe(sass().on('error', sass.logError)) // конвертируем в css
+    .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true })) // Создадим префиксы с помощью Autoprefixer
+    .pipe(cleancss( { level: { 1: { specialComments: 0 } }/* , format: 'beautify' */ } )) // Минифицируем стили
+		.pipe(dest('build/assets/styles')) // складываем в папку билда
+		.pipe(browserSync.stream()) // обновляем вкладку браузера
 }
-function fonts() {
-    return src('src/assets/fonts/**/*')
+
+function fonts()
+{
+  return src('src/assets/fonts/**/*')
     .pipe(dest('build/assets/fonts/'))
     .pipe(browserSync.stream())
 }
 
-async function images() {
-	imagecomp(
-		"src/assets/images/**/*", // Берём все изображения из папки источника
-		"build/assets/images/", // Выгружаем оптимизированные изображения в папку назначения
-		{ compress_force: false, statistic: true, autoupdate: true }, false, // Настраиваем основные параметры
-		{ jpg: { engine: "mozjpeg", command: ["-quality", "75"] } }, // Сжимаем и оптимизируем изображеня
-		{ png: { engine: "pngquant", command: ["--quality=75-100", "-o"] } },
-		{ svg: { engine: "svgo", command: "--multipass" } },
-		{ gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
-		function (err, completed) { // Обновляем страницу по завершению
-			if (completed === true) {
-				browserSync.reload()
-			}
-		}
-	)
+async function images()
+{
+  imagecomp(
+    'src/assets/images/**/*', // Берём все изображения из папки источника
+    'build/assets/images/', // Выгружаем оптимизированные изображения в папку назначения
+    { compress_force: false, statistic: true, autoupdate: true }, false, // Настраиваем основные параметры
+    { jpg: { engine: 'mozjpeg', command: ['-quality', '75'] } }, // Сжимаем и оптимизируем изображеня
+    { png: { engine: 'pngquant', command: ['--quality=75-100', '-o'] } },
+    { svg: { engine: 'svgo', command: '--multipass' } },
+    { gif: { engine: 'gifsicle', command: ['--colors', '64', '--use-col=web'] } },
+    function (err, completed) { // Обновляем страницу по завершению
+      if (completed === true) {
+        browserSync.reload()
+      }
+    },
+  )
 }
 
-function browsersync() {
-    browserSync.init({
-        server: {
-            baseDir: 'build'
-        }
-    })
+function browsersync()
+{
+  browserSync.init({
+    server: {
+      baseDir: 'build',
+    },
+  })
 }
 
-function startWatch() {
-    watch('src/**/*.html', html)
-    watch('src/assets/styles/**/*.css', css)
-    watch('src/assets/images/**/*', images)
-    watch('src/assets/fonts/**/*', fonts)
+function startWatch()
+{
+  watch('src/**/*.html', html)
+  watch('src/assets/styles/**/*.scss', css)
+  watch('src/assets/images/**/*', images)
+  watch('src/assets/fonts/**/*', fonts)
 }
 
-function clear(){
-    return src('build', {read: false})
-    .pipe(clean());
+function clear()
+{
+  return src('build', { read: false })
+    .pipe(clean())
 }
 
-exports.dev = parallel(browsersync, startWatch, html, css, images, fonts)
+exports.dev   = parallel(browsersync, startWatch, html, css, images, fonts)
 exports.build = series(clear, parallel(html, css, images, fonts))
 
 
 exports.default = parallel(browsersync, startWatch, html, css, images, fonts)
 
-  
