6b6ee5580c4f564c879f36e09a50db7b4ef11fd8
Natalia Repkina <pn.88@mail.ru>
2023-06-26 19:11:55 +0300

    fix: slider js

--- src/assets/js/slider.js
+++ src/assets/js/slider.js
@@ -1,24 +1,9 @@
 let Slider = function(gallery){
     this.gallery = document.querySelector(".gallery");
-    this.container = document.querySelector(".speakers__container");
-    this.wrapper = document.querySelector(".gallery__wrapper");
     this.itemList = document.querySelector(".gallery__items-list");
-    this.items = document.querySelectorAll(".gallery__item");
-    this.itemSize = this.items[0].offsetWidth;
     this.buttonLeft = this.gallery.querySelector(".gallery__button_prev");
-    this.buttonRigth = this.gallery.querySelector(".gallery__button_next");
-    this.currentDirection = "right";
-    this.countImg = 3;
+    this.buttonRigth = this.gallery.querySelector(".gallery__button_next");    
     
-    this.positionGallery = function(){
-      let imgWidth = +getComputedStyle(this.gallery.querySelector(".gallery__item")).width.slice(0,-2); 
-      let sliderWidht = imgWidth * this.countImg + (+getComputedStyle(this.gallery.querySelector(".gallery__item")).marginRight.slice(0,-2) * (this.countImg-1));
-      this.gallery.style.width = sliderWidht + "px";
-      if(screen.width > 980){
-        this.gallery.style.marginLeft = -(sliderWidht - this.container.clientWidth/1.8) + "px";
-      }
-    };
-
     let ctx = this;
 
     this.clickOnButtons = function(event){
@@ -28,34 +13,17 @@ let Slider = function(gallery){
           ctx.buttonRigth.classList.toggle("current");
         }
       } 
-      if(event.target == ctx.buttonLeft){
-        ctx.currentDirection = "left";
-        console.log(ctx.currentDirection);
+      if(event.target == ctx.buttonLeft){ 
+        let array = document.querySelectorAll(".gallery__item");
+        ctx.itemList.append(array[0]);
       }
       else if(event.target == ctx.buttonRigth){
-        ctx.currentDirection = "right";
-        console.log(ctx.currentDirection);
+        let array = document.querySelectorAll(".gallery__item");
+        ctx.itemList.prepend(array[array.length - 1]);
       }
-      ctx.sliderRotation();
     };
-    
-    this.sliderRotation = function(){
-      let left = +getComputedStyle(ctx.itemList).marginLeft.slice(0,-2);
-      if(ctx.currentDirection == "left"){
-        if(left<0){
-          ctx.itemList.style.marginLeft = left + ctx.itemSize + 40 + "px";
-        }          
-      }       
-      if(ctx.currentDirection == "right"){
-        if(-left < ctx.itemList.offsetWidth - ctx.wrapper.offsetWidth){
-          ctx.itemList.style.marginLeft = left - ctx.itemSize - 40 + "px";
-        }
-      }
-    }
+}
 
-  }
-  let speakerSlider = new Slider();
+let speakerSlider = new Slider();
 
-  speakerSlider.positionGallery();
-
-  speakerSlider.gallery.addEventListener("click",  speakerSlider.clickOnButtons);
+speakerSlider.gallery.addEventListener("click",  speakerSlider.clickOnButtons);
